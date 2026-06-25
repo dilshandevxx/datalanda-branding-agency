a5fa36cf135d118d6849dfcfdd1dadb47aa03fe2
Dilshan P.A.A.I <dilshaprathapaarachchi@gmail.com>
2026-06-25 18:53:16 +0530

    changed file name

--- src/components/CtaSection.tsx
+++ src/components/CtaSection.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from './CtaSection.module.css';
 import Magnetic from './Magnetic';
 
@@ -5,16 +7,23 @@ export default function CtaSection() {
   return (
     <section id="contact" className={styles.section}>
       <div className={styles.content}>
-        <p className={styles.subtitle}>Have a project in mind?</p>
-        <h2 className={styles.title}>
-          LET'S CONNECT<br />
-          TOGETHER
-        </h2>
+        
+        <div className={styles.textBlock}>
+          <div className={styles.subtitleWrapper}>
+            <div className={styles.line}></div>
+            <p className={styles.subtitle}>Have a project in mind?</p>
+          </div>
+          
+          <h2 className={styles.title}>
+            Let's build something <br />
+            <i>extraordinary.</i>
+          </h2>
+        </div>
         
         <div className={styles.actionWrapper}>
           <Magnetic>
-            <a href="mailto:hello@datalands.com" className={styles.button}>
-              hello@datalands.com <span className={styles.arrow}>↗</span>
+            <a href="mailto:hello@datalands.com" className={styles.circleButton}>
+              <span className={styles.btnText}>Get in<br />Touch</span>
             </a>
           </Magnetic>
         </div>
