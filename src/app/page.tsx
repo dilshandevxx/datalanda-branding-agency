258d2fcd9c8353f64f547bd4d92a2a83da9fddba
Dilshan P.A.A.I <dilshaprathapaarachchi@gmail.com>
2026-06-25 19:00:58 +0530

    changed file name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,9 @@ export default function Home() {
       <StudioSection />
       <SpotlightSection />
       <TestimonialSection />
-      <FaqSection />
+      <ThemeInversionController>
+        <FaqSection />
+      </ThemeInversionController>
       <CtaSection />
       <FooterSection />
     </main>
