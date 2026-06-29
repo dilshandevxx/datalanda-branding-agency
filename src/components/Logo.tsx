18c66d64036fb37911aa6d2bf0f95c5813775a41
Dilshan P.A.A.I <dilshaprathapaarachchi@gmail.com>
2026-06-29 23:11:54 +0530

    changed file name

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 export const Logo = ({ className }: { className?: string }) => (
   <svg 
-    viewBox="0 0 24 24" 
+    viewBox="0 0 32 32" 
     className={className}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    style={{ height: '1.4em', width: 'auto', display: 'inline-block', verticalAlign: 'middle', marginRight: '10px', color: 'inherit' }}
+    style={{ height: '2.5rem', width: 'auto', display: 'inline-block', verticalAlign: 'middle', color: 'inherit' }}
   >
-    <path d="M 8 2 H 24 L 20 10 H 12 L 10 14 H 2 Z" fill="currentColor" />
-    <path d="M 14 10 H 22 L 16 22 H 0 L 4 14 H 12 Z" fill="currentColor" opacity="0.5" />
+    <mask id="skylabs-s-mask">
+      <path d="M 0 0 H 24 L 32 8 V 32 H 8 L 0 24 Z" fill="white" />
+      <path d="M 26 3.5 H 6 V 18.5 H 21 V 23.5 H 6 V 28.5 H 26 V 13.5 H 11 V 8.5 H 26 Z" fill="black" />
+    </mask>
+    <rect width="32" height="32" fill="currentColor" mask="url(#skylabs-s-mask)" />
   </svg>
 );
