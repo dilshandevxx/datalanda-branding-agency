44570f3618dbb87c20b7f4a179f262c4ca8dbd02
Dilshan P.A.A.I <dilshaprathapaarachchi@gmail.com>
2026-06-29 09:37:35 +0530

    changed file name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
-        {/* We use default sans-serif for Geist since modern browsers have great defaults, or you can add Geist CDN here */}
+        <link href="https://fonts.googleapis.com/css2?family=Geist:wght@300;400;500;600;700;800;900&family=Inter:wght@300;400;500;600;700;800;900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
       </head>
       <body>
         <LazyMotion features={domAnimation}>
